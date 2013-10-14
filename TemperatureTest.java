ab932f809c1b63317916871a8d6840cb273c0117
Joanna HALPERN <jhalpe5@open-13.CS.McGill.CA>
2013-10-14 19:23:10 -0400

    Testing if program allows temperature below absolute zero

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -46,31 +46,23 @@ public class TemperatureTest {
 		assertEquals("get value doesn't return the expected value", expectedValue, actualValue, 0.000001);
 	}
 
-//	public void test_changeUnits(Temperature.Units unitsFrom, double valueFrom, Temperature.Units unitsTo, double valueTo){
-//		Temperature boilingInC = new Temperature(100, Temperature.Units.CELSIUS);
-//		Temperature boilingInF = new Temperature(212, Temperature.Units.FAHRENHEIT);
-//		Temperature boilingInK = new Temperature(373.15, Temperature.Units.KELVIN);
-//		
-//		Temperature.Units expectedUnits = Temperature.Units.FAHRENHEIT;
-//
-//	}
-	
 	public void test_changeUnits(Temperature temperatureFrom, Temperature temperatureTo){ //meaning Temperature object from
 
 		Temperature.Units expectedUnits = temperatureTo.getUnits();
 		double expectedValue = temperatureTo.getValue();
 		
 		temperatureFrom.changeUnits(expectedUnits);
+		
 		Temperature.Units actualUnits = temperatureFrom.getUnits();
 		double actualValue = temperatureFrom.getValue();
 		
 		assertTrue(expectedUnits == actualUnits);
+		assertEquals(expectedValue, actualValue, 0.000001);
+
 		System.out.println("expectedUnits:" + expectedUnits);
 		System.out.println("actualUnits:" + actualUnits);
 		System.out.println("expectedValue:" + expectedValue);
 		System.out.println("actualValue:" + actualValue);
-
-		assertEquals(expectedValue, actualValue, 0.000001);
 	}
 	
 	@Test
@@ -103,10 +95,12 @@ public class TemperatureTest {
 		test_changeUnits(boilingInK, boilingInF);
 		test_changeUnits(absoluteZeroInK, absoluteZeroInF);
 	}
-//	@Test
-//	public void test_unitsAfterConversion
-//	Temperature boilingInCelsius = new Temperature (100, Temperature.CELSIUS);
-	
+	@Test(expected = Exception.class)
+	public void belowAbsoluteZero(){
+		Temperature belowAbZero = new Temperature(-5, Temperature.Units.KELVIN);
+		fail();		
+	}
+
   // Add multiple tests to check all functions of
   // {@Code Temperature} class.
 
